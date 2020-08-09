ad77e7395f61994ad79a7a5c1642f0da18b3f988
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-09 14:52:43 +0800

    ADD PATCH , DELETE and GET routes

--- src/controllers/users/users.controller.ts
+++ src/controllers/users/users.controller.ts
@@ -10,13 +10,23 @@ import { UsersServices } from "./users.service";
 export class UsersController {
   constructor(private services: UsersServices) {}
 
+  @Get({ path: "/:id", middlewares: [] })
+  async byId(req: Request, res: Response, next: NextFunction): Promise<void> {
+    res.send(await this.services.byID(req.params.id));
+  }
+
   @Post({ path: "/", middlewares: [] })
   async addUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     res.send(await this.services.create(req.body));
   }
 
-  @Get({ path: "/", middlewares: [] })
-  async allUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-    res.send(await this.services.all());
+  @Patch({ path: "/:id", middlewares: [] })
+  async updateUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    res.send(await this.services.update(req.params.id, req.body));
+  }
+
+  @Delete({ path: "/:id", middlewares: [] })
+  async deleteUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    res.send(await this.services.delete(req.params.id));
   }
 }

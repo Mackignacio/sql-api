47d8535cfe70d9d6c0e42537394b6310375f242c
Mac Ignacio <mack_ignacio@yahoo.com>
2020-08-09 14:45:27 +0800

    Add create method in user service

--- src/controllers/users/users.service.ts
+++ src/controllers/users/users.service.ts
@@ -1,10 +1,18 @@
-import { Injectable } from "@mayajs/core";
+import { ModelCtor, Model, Sequelize } from "sequelize";
+import { Injectable, Database } from "@mayajs/core";
+import { Models } from "@mayajs/sql";
 
 @Injectable()
 export class UsersServices {
+  @Models("user") model!: ModelCtor<Model<any, any>>;
+
   constructor() {}
 
-  hello() {
-    return "Hello world!";
+  async create(body: any) {
+    try {
+      return this.model.create(body);
+    } catch (error) {
+      return error;
+    }
   }
 }
